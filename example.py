080dbef0a00689437566f5b48014387d4bf30517
LittleSaltFish <saltfish@whut.edu.cn>
2021-12-30 18:33:39 +0800

    mod code

--- example.py
+++ example.py
@@ -1,19 +1,31 @@
 import asyncio
 import danmaku
 import sys
+import time
+import json
 
 
-async def printer(q):
+async def printer(q,link):
     while True:
         m = await q.get()
-        print(m)
 
+        Now=time.time()
+        m["Date"]=time.strftime('%Y.%m.%d',time.localtime(time.time()))
+        m["ExactTime"]=time.strftime('%H:%M:%S',time.localtime(time.time()))
+        m["DeltaTime"]=round(Now-StartTime,2)
+
+        RoomId=link.split("/")[-1]
+        with open(f"./{RoomId}.json","a+",encoding="utf-8") as f:
+            data=m
+            # data=json.dumps(m)
+            f.write(str(data)+"\n")
+        print(data)
 
 async def main():
     q = asyncio.Queue()
     dmc = danmaku.DanmakuClient(sys.argv[1], q)
-    asyncio.create_task(printer(q))
+    asyncio.create_task(printer(q,sys.argv[1]))
     await dmc.start()
 
-
+StartTime=time.time()
 asyncio.run(main())
