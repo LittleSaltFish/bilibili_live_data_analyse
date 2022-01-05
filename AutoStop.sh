5399996f48caad58f032ffb40df22b2e4968ac83
LittleSaltFish <saltfish@whut.edu.cn>
2022-01-05 17:07:55 +0800

    更改了stop的写法，使不易出错

--- AutoStop.sh
+++ AutoStop.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 for line in $(ps -aux|grep -v "grep" |grep "python3 ./example.py https://live.bilibili.com/"|awk '{print $2}')
 do
-    # echo $line
+    echo $line
     kill -9 $line
     echo "Successfully Stop AutoFetch : $line"
 done
 
-​kill -9 $(ps -aux |grep -v "grep"|grep AutoMessage.sh|awk '{print $2}')
-echo "Successfully Stop AutoMessage : $line"
+for line in $(ps -aux |grep -v "grep"|grep "AutoMessage.sh"|awk '{print $2}')
+do
+    kill -9 $line
+    echo "Successfully Stop AutoMessage : $line"
+done
 
 echo "Done!"
 
